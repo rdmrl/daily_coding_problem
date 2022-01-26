3a4b214ccb20d3b454c564e9c84cc8c5af1975c1
Ravi Damarla <ravi.damarla@gmail.com>
2022-01-26 14:36:29 +0000

    formatted the code

--- 860_regex_match/regex_match.js
+++ 860_regex_match/regex_match.js
@@ -20,134 +20,133 @@
  */
 
 class ParseTree {
-	constructor(regexp) {
-	  this.regexp = regexp;
-		this.hasPeriod = false;
-		this.hasAsterisk = false;
-		this.prefixArr = [];
-		this.suffixArr = [];
-	}
+  constructor( regexp ) {
+    this.regexp = regexp;
+    this.hasPeriod = false;
+    this.hasAsterisk = false;
+    this.prefixArr = [];
+    this.suffixArr = [];
+  }
 
-  addSuffix(c) {
-	  this.suffixArr.push(c);
-	}
+  addSuffix( c ) {
+    this.suffixArr.push( c );
+  }
 
-  addPrefix(c) {
-	  this.prefixArr.push(c);
-	}
+  addPrefix( c ) {
+    this.prefixArr.push( c );
+  }
 
-	onlyHasPeriod() {
-		return (this.hasPeriod) && !(this.hasAsterisk);
-	}
+  onlyHasPeriod() {
+    return ( this.hasPeriod ) && !( this.hasAsterisk );
+  }
 
-	onlyHasAsterisk() {
-		return !(this.hasPeriod) && (this.hasAsterisk);
-	}
+  onlyHasAsterisk() {
+    return !( this.hasPeriod ) && ( this.hasAsterisk );
+  }
 
-	hasSuffix() {
-	  return ( this.suffixArr.length > 0 );
-	}
+  hasSuffix() {
+    return ( this.suffixArr.length > 0 );
+  }
 
-	getSuffix() {
-		return this.suffixArr.join('');
-	}
+  getSuffix() {
+    return this.suffixArr.join( '' );
+  }
 
-	hasPrefix() {
-	  return ( this.prefixArr.length > 0 );
-	}
+  hasPrefix() {
+    return ( this.prefixArr.length > 0 );
+  }
 
-	getPrefix() {
-		return this.prefixArr.join('');
-	}
+  getPrefix() {
+    return this.prefixArr.join( '' );
+  }
 }
 
-let buildParser = function(regexp) {
+let buildParser = function( regexp ) {
 
-	let parser = new ParseTree(regexp);
+  let parser = new ParseTree( regexp );
 
-	// Loop through the characters.
+  // Loop through the characters.
   for ( let c of regexp ) {
-		if ( c === '.' ) {
-			parser.hasPeriod = true;
-		} else if ( c === '*' ) {
-			parser.hasAsterisk = true;
-		} else {
-			if ( parser.hasPeriod || parser.hasAsterisk ) {
-				parser.addSuffix( c );
-			} else {
-				parser.addPrefix( c );
-			}
-		}
-	}
+    if ( c === '.' ) {
+      parser.hasPeriod = true;
+    } else if ( c === '*' ) {
+      parser.hasAsterisk = true;
+    } else {
+      if ( parser.hasPeriod || parser.hasAsterisk ) {
+        parser.addSuffix( c );
+      } else {
+        parser.addPrefix( c );
+      }
+    }
+  }
 
-	// console.log( 'parser: ', parser );
+  // console.log( 'parser: ', parser );
 
-	return parser;
+  return parser;
 };
 
 let parsers = [];
 
-let getParser = function(regexp) {
+let getParser = function( regexp ) {
 
-	if (!parsers[regexp]) {
-		parsers[regexp] = buildParser(regexp);
-	}
-		
-	return parsers[regexp];
+  if ( !parsers[ regexp ] ) {
+    parsers[ regexp ] = buildParser( regexp );
+  }
+
+  return parsers[ regexp ];
 };
 
 module.exports = {
-  match: function(regexp, inputString) {
+  match: function( regexp, inputString ) {
 
-		console.log( inputString );
-		let parser = getParser(regexp);
+    console.log( inputString );
+    let parser = getParser( regexp );
 
-		// Case A: regex has only a period
-		// Case B: regex has only an asterisk
-		// Case C: regex has both patterns
+    // Case A: regex has only a period
+    // Case B: regex has only an asterisk
+    // Case C: regex has both patterns
 
-		// console.log( 'hasOnlyPeriod: ' + parser.onlyHasPeriod() + ', hasOnlyAsterisk: ' + parser.onlyHasAsterisk() );
-		// console.log( 'hasSuffix: ' + parser.hasSuffix() + ', hasPrefix: ' + parser.hasPrefix() );
+    // console.log( 'hasOnlyPeriod: ' + parser.onlyHasPeriod() + ', hasOnlyAsterisk: ' + parser.onlyHasAsterisk() );
+    // console.log( 'hasSuffix: ' + parser.hasSuffix() + ', hasPrefix: ' + parser.hasPrefix() );
 
-		// 1. xyz.  
-		// 2. .zyx  
-		// 3. xy.z
-		const prefixStr = parser.getPrefix();
-		const suffixStr = parser.getSuffix();
+    // 1. xyz.  
+    // 2. .zyx  
+    // 3. xy.z
+    const prefixStr = parser.getPrefix();
+    const suffixStr = parser.getSuffix();
 
-		if ( parser.onlyHasPeriod() ) {
-			if ( parser.hasPrefix() ) {
-				if ( ! parser.hasSuffix() ) {
-					// Case A.1
-					console.log( 'Case A.1' );
-					if ( inputString.indexOf( prefixStr ) === 0 ) {
-						if ( ( prefixStr.length + 1 ) === inputString.length ) {
-							return true;
-						}
-					}
-				} else {
-					// Case A.3
-					console.log( 'Case A.3' );
-					if ( inputString.indexOf( prefixStr ) === 0 ) {
-						if ( inputString.indexOf( suffixStr ) === ( prefixStr.length + 1 ) ) {
-							if ( ( prefixStr.length + suffixStr.length + 1 ) === inputString.length ) { 
-								return true;
-							}
-						}
-					}
-				}
-			} else {
-				// Case A.2
-				console.log ( 'Case A.2' );
-				if ( inputString.indexOf( suffixStr ) === 1 ) {
-					if ( (suffixStr.length + 1 ) === inputString.length ) {
-						return true;
-					}
-				}
-			}
-		}
+    if ( parser.onlyHasPeriod() ) {
+      if ( parser.hasPrefix() ) {
+        if ( !parser.hasSuffix() ) {
+          // Case A.1
+          console.log( 'Case A.1' );
+          if ( inputString.indexOf( prefixStr ) === 0 ) {
+            if ( ( prefixStr.length + 1 ) === inputString.length ) {
+              return true;
+            }
+          }
+        } else {
+          // Case A.3
+          console.log( 'Case A.3' );
+          if ( inputString.indexOf( prefixStr ) === 0 ) {
+            if ( inputString.indexOf( suffixStr ) === ( prefixStr.length + 1 ) ) {
+              if ( ( prefixStr.length + suffixStr.length + 1 ) === inputString.length ) {
+                return true;
+              }
+            }
+          }
+        }
+      } else {
+        // Case A.2
+        console.log( 'Case A.2' );
+        if ( inputString.indexOf( suffixStr ) === 1 ) {
+          if ( ( suffixStr.length + 1 ) === inputString.length ) {
+            return true;
+          }
+        }
+      }
+    }
 
-		return false;
-	}
+    return false;
+  }
 };
-
