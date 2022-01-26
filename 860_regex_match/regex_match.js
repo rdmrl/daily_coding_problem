e6b91bbe2ed8de565027b0261e95f91069f977c2
Ravi Damarla <ravi.damarla@gmail.com>
2022-01-26 14:33:12 +0000

    implemented pattern matching

--- 860_regex_match/regex_match.js
+++ 860_regex_match/regex_match.js
@@ -19,32 +19,70 @@
  * ======
  */
 
+class ParseTree {
+	constructor(regexp) {
+	  this.regexp = regexp;
+		this.hasPeriod = false;
+		this.hasAsterisk = false;
+		this.prefixArr = [];
+		this.suffixArr = [];
+	}
+
+  addSuffix(c) {
+	  this.suffixArr.push(c);
+	}
+
+  addPrefix(c) {
+	  this.prefixArr.push(c);
+	}
+
+	onlyHasPeriod() {
+		return (this.hasPeriod) && !(this.hasAsterisk);
+	}
+
+	onlyHasAsterisk() {
+		return !(this.hasPeriod) && (this.hasAsterisk);
+	}
+
+	hasSuffix() {
+	  return ( this.suffixArr.length > 0 );
+	}
+
+	getSuffix() {
+		return this.suffixArr.join('');
+	}
+
+	hasPrefix() {
+	  return ( this.prefixArr.length > 0 );
+	}
+
+	getPrefix() {
+		return this.prefixArr.join('');
+	}
+}
+
 let buildParser = function(regexp) {
 
-	let prefix = [];
-	let suffix = [];
-
-	let foundPeriod = false;
-	let foundAsterisk = false;
+	let parser = new ParseTree(regexp);
 
 	// Loop through the characters.
   for ( let c of regexp ) {
 		if ( c === '.' ) {
-			foundPeriod = true;
+			parser.hasPeriod = true;
 		} else if ( c === '*' ) {
-			foundAsterisk = true;
+			parser.hasAsterisk = true;
 		} else {
-			if ( foundPeriod || foundAsterisk ) {
-				suffix.push( c );
+			if ( parser.hasPeriod || parser.hasAsterisk ) {
+				parser.addSuffix( c );
 			} else {
-				prefix.push( c );
+				parser.addPrefix( c );
 			}
 		}
 	}
 
-	console.log( [prefix, suffix, foundPeriod, foundAsterisk] );
+	// console.log( 'parser: ', parser );
 
-	return [prefix, suffix, foundPeriod, foundAsterisk];
+	return parser;
 };
 
 let parsers = [];
@@ -61,7 +99,53 @@ let getParser = function(regexp) {
 module.exports = {
   match: function(regexp, inputString) {
 
+		console.log( inputString );
 		let parser = getParser(regexp);
+
+		// Case A: regex has only a period
+		// Case B: regex has only an asterisk
+		// Case C: regex has both patterns
+
+		// console.log( 'hasOnlyPeriod: ' + parser.onlyHasPeriod() + ', hasOnlyAsterisk: ' + parser.onlyHasAsterisk() );
+		// console.log( 'hasSuffix: ' + parser.hasSuffix() + ', hasPrefix: ' + parser.hasPrefix() );
+
+		// 1. xyz.  
+		// 2. .zyx  
+		// 3. xy.z
+		const prefixStr = parser.getPrefix();
+		const suffixStr = parser.getSuffix();
+
+		if ( parser.onlyHasPeriod() ) {
+			if ( parser.hasPrefix() ) {
+				if ( ! parser.hasSuffix() ) {
+					// Case A.1
+					console.log( 'Case A.1' );
+					if ( inputString.indexOf( prefixStr ) === 0 ) {
+						if ( ( prefixStr.length + 1 ) === inputString.length ) {
+							return true;
+						}
+					}
+				} else {
+					// Case A.3
+					console.log( 'Case A.3' );
+					if ( inputString.indexOf( prefixStr ) === 0 ) {
+						if ( inputString.indexOf( suffixStr ) === ( prefixStr.length + 1 ) ) {
+							if ( ( prefixStr.length + suffixStr.length + 1 ) === inputString.length ) { 
+								return true;
+							}
+						}
+					}
+				}
+			} else {
+				// Case A.2
+				console.log ( 'Case A.2' );
+				if ( inputString.indexOf( suffixStr ) === 1 ) {
+					if ( (suffixStr.length + 1 ) === inputString.length ) {
+						return true;
+					}
+				}
+			}
+		}
 
 		return false;
 	}
