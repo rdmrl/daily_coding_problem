09babb6bc3eeac7287e2625f48f440007bde6324
Ravi Damarla <ravi.damarla@gmail.com>
2022-01-26 16:53:46 +0000

    added unit tests for wildcard pattern match

--- 860_regex_match/index.test.js
+++ 860_regex_match/index.test.js
@@ -2,13 +2,13 @@ let matcher = require('./regex_match');
 
 let assert = require('assert');
 describe('RegexMatch', function () {
-	describe('RegexMatchPeriod1', function () {
+	describe('RegexMatchPeriod1Pass', function () {
 		it("should return true when passed 'ra.' and 'ray'", function () {
 			assert.equal( matcher.match('ra.', 'ray' ), true );
 		} );
 	} );
 
-	describe('RegexMatchPeriod2', function () {
+	describe('RegexMatchPeriod2Fail', function () {
 		it("should return false when passed 'ra.' and 'raymond'", function () {
 			assert.equal( matcher.match('ra.', 'raymond' ), false );
 		} );
@@ -32,16 +32,53 @@ describe('RegexMatch', function () {
 		} );
 	} );
 
-	describe('RegexMatchPeriod5Pass', function () {
+	describe('RegexMatchPeriod6Pass', function () {
 		it("should return true when passed 'mid.ay' and 'midday'", function () {
 			assert.equal( matcher.match('mid.ay', 'midday' ), true );
 		} );
 	} );
 
-	describe('RegexMatchPeriod6Fail', function () {
+	describe('RegexMatchPeriod7Fail', function () {
 		it("should return false when passed 'mid.ay' and 'middays'", function () {
 			assert.equal( matcher.match('mid.ay', 'middays' ), false );
 		} );
 	} );
+
+	describe('RegexMatchAsterisk1Pass', function () {
+		it("should return true when passed '.*at' and 'chat'", function () {
+			assert.equal( matcher.match('.*at', 'chat' ), true );
+		} );
+	} );
+
+	describe('RegexMatchAsterisk1Fail', function () {
+		it("should return false when passed '.*at' and 'chats'", function () {
+			assert.equal( matcher.match('.*at', 'chats' ), false );
+		} );
+	} );
+
+	describe('RegexMatchAsterisk2Pass', function () {
+		it("should return true when passed 'at.*' and 'attempt'", function () {
+			assert.equal( matcher.match('at.*', 'attempt' ), true );
+		} );
+	} );
+
+	describe('RegexMatchAsterisk2Fail', function () {
+		it("should return false when passed 'at.*' and 'later'", function () {
+			assert.equal( matcher.match('at.*', 'later' ), false );
+		} );
+	} );
+
+	describe('RegexMatchAsterisk3Pass', function () {
+		it("should return true when passed 'at.*er' and 'atelier'", function () {
+			assert.equal( matcher.match('at.*er', 'atelier' ), true );
+		} );
+	} );
+
+	describe('RegexMatchAsterisk3Fail', function () {
+		it("should return false when passed 'at.*er' and 'ateliers'", function () {
+			assert.equal( matcher.match('at.*er', 'ateliers' ), false );
+		} );
+	} );
+
 
 } );
