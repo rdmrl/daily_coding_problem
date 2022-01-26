47113858effe0f4231010665366addd50e40238c
Ravi Damarla <ravi.damarla@gmail.com>
2022-01-26 14:32:57 +0000

    added debug output

--- 860_regex_match/app.js
+++ 860_regex_match/app.js
@@ -1,10 +1,15 @@
 let matcher = require('./regex_match');
 
+console.log( "-------------------------------\n" );
 const matchVal = matcher.match('ra.', 'ray');
 console.log( 'ra. => ray : ', matchVal );
 
+console.log( "-------------------------------\n" );
 const matchVal2 = matcher.match('ra.', 'raymond');
 console.log( 'ra. => raymond : ', matchVal2 );
 
+/*
+console.log( "-------------------------------\n" );
 const matchVal3 = matcher.match('.*at', 'chat');
 console.log( '.*at => chat : ', matchVal3 );
+*/
